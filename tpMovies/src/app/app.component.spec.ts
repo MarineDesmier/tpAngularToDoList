c7d9b928b66bb085f36d09363b1483540feafbd9
Marine Desmier <desmier.marine@bbox.fr>
2023-03-29 11:38:42 +0200

    mise en place header+footer+ shared

--- tpMovies/src/app/app.component.spec.ts
+++ tpMovies/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'toDoList'`, () => {
+  it(`should have as title 'tpMovies'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('toDoList');
+    expect(app.title).toEqual('tpMovies');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('toDoList app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tpMovies app is running!');
   });
 });
--- tpMovies/src/app/app.module.ts
+++ tpMovies/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
